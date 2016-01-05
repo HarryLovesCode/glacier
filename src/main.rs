37e7950d5e25352354c929c89e31fd535dc49790
Harrison G <Harrison.Gould16@Gmail.com>
2016-01-05 17:47:21 -0500

    Minor changes with args and data Arc Mutex

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 extern crate image;
+extern crate num_cpus;
 extern crate rand;
 
 mod geometry;
@@ -9,24 +10,44 @@ use geometry::*;
 use material::*;
 use math::*;
 use rand::random;
+use std::env;
 use std::path::Path;
-use std::sync::mpsc;
+use std::sync::{Arc, mpsc, Mutex};
 use std::thread;
 
-const NUM_THREADS: usize = 4;
+const DEFAULT_SAMPLES: usize = 8;
+const DEFAULT_W_PIXELS: usize = 512;
+const DEFAULT_H_PIXELS: usize = 512;
 
 fn main() {
-    let w = 512;
-    let h = 512;
+    let args: Vec<String> = env::args().collect();
+    let mut w = DEFAULT_W_PIXELS;
+    let mut h = DEFAULT_H_PIXELS;
+    let mut s = DEFAULT_SAMPLES;
+
+    match args.len() {
+        2 => s = args[1].parse().unwrap(),
+        4 => {
+            s = args[1].parse().unwrap();
+            w = args[2].parse().unwrap();
+            h = args[3].parse().unwrap();
+        },
+        _ => {},
+    }
+
+    s /= 4;
+
     let w_f = w as f64;
     let h_f = h as f64;
-    let samps = 4;
-    let chunk_h = h / NUM_THREADS;
+    let s_f = s as f64;
+    let n = num_cpus::get();
+
+    let chunk_h = h / n;
     let cam = Ray::new(Point::new(50.0, 52.0, 295.6), Vector::new(0.0, -0.042612, -1.0).norm());
     let cx = Vector::new(w as f64 * 0.5135 / h as f64, 0.0, 0.0);
     let cy = cx.cross(cam.dir).norm() * 0.5135;
     let (tx, rx) = mpsc::channel();
-    let mut out = vec![0u8; 3 * w * h];
+    let data = Arc::new(Mutex::new(vec![0u8; 3 * w * h]));
 
     let materials = [
         Material::new(Color::zero(),    Color::new(0.75, 0.25, 0.25), MaterialType::DIFFUSE),
@@ -50,11 +71,10 @@ fn main() {
         Sphere::new(600.0,     Point::new(50.0, 681.33, 81.6),    materials[6]),
     ];
 
-    for t in 0..NUM_THREADS {
-        let tx = tx.clone();
+    for t in 0..n {
+        let (data, tx) = (data.clone(), tx.clone());
 
         thread::spawn(move || {
-            let mut data = Vec::with_capacity(3 * w * chunk_h);
             let bot = chunk_h * t;
             let top = chunk_h * (t + 1);
             for y in bot..top {
@@ -68,7 +88,7 @@ fn main() {
                         for sx in 0..2 {
                             let sx = sx as f64;
                             let sy = sy as f64;
-                            for _ in 0..samps {
+                            for _ in 0..s {
                                 let r1 = 2.0 * random::<f64>();
                                 let r2 = 2.0 * random::<f64>();
                                 let dx = if r1 < 1.0 { r1.sqrt() - 1.0 } else { 1.0 - (2.0 - r1).sqrt() };
@@ -81,27 +101,24 @@ fn main() {
                         }
                     }
 
-                    accum = accum * (0.25 / samps as f64);
-                    data.push(to_int(accum.r));
-                    data.push(to_int(accum.g));
-                    data.push(to_int(accum.b));
+                    let mut data = data.lock().unwrap();
+                    let pix_loc = (h - y - 1) * w * 3 + x * 3;
+
+                    accum = accum * (0.25 / s_f);
+                    data[pix_loc + 0] = to_int(accum.r);
+                    data[pix_loc + 1] = to_int(accum.g);
+                    data[pix_loc + 2] = to_int(accum.b);
                 }
             }
 
-            let pix_loc = (h - bot) * w * 3;
-            let _ = tx.send((pix_loc - 1, data));
+            let _ = tx.send(());
         });
     }
 
-    for _ in 0..NUM_THREADS {
-        let (s_idx, data) = rx.recv().unwrap();
-
-        for (idx, val) in data.iter().enumerate() {
-            out[s_idx - idx] = *val;
-        }
+    for _ in 0..n {
+        let _ = rx.recv().unwrap();
     }
 
-    let w = w as u32;
-    let h = h as u32;
-    let _ = image::save_buffer(&Path::new("smallpt.png"), &out, w, h, image::RGB(8));
+    let (w, h, data) = (w as u32, h as u32, data.lock().unwrap());
+    let _ = image::save_buffer(&Path::new("smallpt.png"), &data, w, h, image::RGB(8));
 }
